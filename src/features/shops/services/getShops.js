a352fead24e9a7be2ec64080e4da7474eb4d8707
Ruben Castillo <runbex13@gmail.com>
2025-10-16 12:46:04 -0400

    refactor(geolocation): Centralizar la lógica para obtener coordenadas
    Se ha refactorizado la obtención de las coordenadas de geolocalización del usuario para eliminar la duplicación de código.
    
    Anteriormente, la función para acceder a `navigator.geolocation` estaba definida localmente tanto en `getShopDetails.js` como en `getShops.js`.
    
    Este commit introduce un nuevo archivo de utilidad, `getCoordsCustomer.js`, que centraliza esta lógica en una única función reutilizable. Los servicios `getShopDetails` y `getAllShops` ahora importan y utilizan esta función, lo que mejora la mantenibilidad y sigue el principio DRY (Don't Repeat Yourself).

--- src/features/shops/services/getShops.js
+++ src/features/shops/services/getShops.js
@@ -1,34 +1,17 @@
 //api
 import api from '../../../lib/axios';
+//hooks
 import useLoaderStore from '../../../store/loaderStore';
+//lib / utils
+import getCoordsCustomer from '../../../utils/getCoordsCustomer';
+
 const getAllShops = async (setShops) => {
   const { showLoader, hideLoader } = useLoaderStore.getState();
   showLoader();
 
-  // Función para obtener coordenadas como una Promesa
-  const getCoords = () => {
-    return new Promise((resolve, reject) => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            resolve({
-              lat: position.coords.latitude,
-              lon: position.coords.longitude,
-            });
-          },
-          (error) => {
-            reject(error); // Rechaza la promesa si hay un error
-          }
-        );
-      } else {
-        reject(new Error('Geolocalización no soportada por el navegador.'));
-      }
-    });
-  };
-
   try {
     // Intenta obtener las coordenadas
-    const coordsCustomer = await getCoords();
+    const coordsCustomer = await getCoordsCustomer();
     //requests
     /* 
       Cordenadas para test
